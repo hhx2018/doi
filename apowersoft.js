7cc1fde3a6ee847e826cad155d0da4a36e6ac93d
HuanHuBear <51444200+hhx2018@users.noreply.github.com>
2022-12-28 01:56:11 +0800

    Add files via upload

---
+++ apowersoft.js
@@ -0,0 +1,44 @@
+/***********************************
+
+> 应用名称：傲软抠图
+> 软件版本：1.6.0
+> 下载地址：https://apps.apple.com/cn/app/id1490054676
+
+ 
+[rewrite_local]
+
+# ～ 傲软抠图解锁会员权限（2022-09-13）
+https?:\/\/.*\.aoscdn\.com\/base\/vip\/client\/authorizations$ url script-response-body https://raw.githubuserc
+
+[mitm] 
+
+hostname=*.aoscdn.com
+
+***********************************/
+
+
+
+var cuttlefish ={"warning":"","tgchannel":"https:1","feedback":"htt"};
+var ddgksf2013 = {
+  "status" : 200,
+  "message" : "success",
+  "data" : {
+    "expired_at" : 4045798296,
+    "is_activated" : 1,
+    "is_lifetime" : 1,
+    "expire_time" : "2099-01-01 00:00:00",
+    "device_id" : 600150864,
+    "period_type" : "active",
+    "remain_days" : 99999,
+    "product_id" : 369,
+    "has_present" : 0,
+    "allowed_device_count" : 1,
+    "has_buy_extend" : 0,
+    "will_expire" : 0,
+    "license_type" : "premium",
+    "begin_activated_time" : 1645798296,
+    "durations" : 0,
+    "vip_special" : 1
+  }
+};
+$done({body: JSON.stringify(ddgksf2013)});
